3e549af430dc6a4644ed4341e85ff2fa749fcb44
james tak <jtak93@gmail.com>
2016-11-30 13:49:09 -0800

    create contact route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,3 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Contact = require('../../models/contact')
+
+router.post('/contacts/create', createContact)
+
+function createContact() {
+  console.log(req.body)
+}
